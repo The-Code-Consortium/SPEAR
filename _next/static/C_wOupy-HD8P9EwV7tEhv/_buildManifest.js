3af52381fea53d0f6dcce25e05e2cc57f107cdfe
ManukaYashan <ManukaYashan@users.noreply.github.com>
2026-09-06 12:22:15 +0000

    deploy: 2c99ccfd9dee47b1074f5049a5be19c7d21ce0e6

---
+++ _next/static/C_wOupy-HD8P9EwV7tEhv/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/SPEAR//_next/:path+",
+        "destination": "/SPEAR/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
